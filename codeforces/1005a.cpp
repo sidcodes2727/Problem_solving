23391eeb6115ce1a24fe33acf0677f4d0727d594
Siddhant <siddhantjssalunke@gmail.com>
2025-10-24 17:54:39 +0530

    hie

---
+++ codeforces/1005a.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+const int MOD = 1e9+7;
+
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+
+int modpow(int a, int b, int mod = MOD) {
+    int res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+vector<int> sieve(int n) {
+    vector<int> isPrime(n + 1, 1);
+    isPrime[0] = isPrime[1] = 0;
+    for (int i = 2; i*i <= n; i++)
+        if (isPrime[i])
+            for (int j = i*i; j <= n; j += i)
+                isPrime[j] = 0;
+    return isPrime;
+}
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+bool allZero(string s){
+    for(auto &i:s){
+        if(i=='1')   return 0;
+    }
+    return 1;
+}
+void solve(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int i;
+    if(allZero(s)){
+        cout<<0<<endl;
+        return;
+    }
+    for(i=0;i<n;i++){
+        if(s[i]=='1')   break;
+    }
+
+    while(s[i]=='1' && i<n) i++;
+
+    cout<<n-(i-1)<<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin>>t;
+    while (t--)
+    {
+      solve();
+    }
+}
