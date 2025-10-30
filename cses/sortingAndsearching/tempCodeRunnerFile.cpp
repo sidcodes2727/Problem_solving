3eac5287c1c61a6a377ac0bfee6bf6d3eebd3660
Siddhant <siddhantjssalunke@gmail.com>
2025-10-30 16:35:50 +0530

    sorting and fun

---
+++ cses/sortingAndsearching/tempCodeRunnerFile.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+const int MOD = 1e9+7;
+
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+
+int modpow(int a, int b, int mod = MOD) {
+    int res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+vector<int> sieve(int n) {
+    vector<int> isPrime(n + 1, 1);
+    isPrime[0] = isPrime[1] = 0;
+    for (int i = 2; i*i <= n; i++)
+        if (isPrime[i])
+            for (int j = i*i; j <= n; j += i)
+                isPrime[j] = 0;
+    return isPrime;
+}
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+void solve(){
+    int n,m,k;
+    cin>>n>>m>>k;
+    vector<int> a(n);
+    vector<int> b(m);
+    for(int i=0;i<n;i++)    cin>>a[i];
+    for(int i=0;i<m;i++)    cin>>b[i];
+    int i=0;
+    int j=0;
+    int cnt =0;
+    while(i<n && j<m){
+        int l=a[i]-k;
+        int u=a[i]+k;
+        if(b[j]<l){
+            j++;
+        }
+        else if(b[j]>u){
+            i++;
+        }
+        else{
+            cnt++;
+            i++;
+        }
+    }
+    cout<<cnt<<endl;
+}   
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+      solve();
+    }
+}
